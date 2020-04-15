ccdaea549589b4a7c77bc1c0d8d16ed7289d9120
batro83 <batro_ska@yahoo.es>
2020-04-15 22:15:31 +0200

    Initial commit

--- src/main/java/com/gtwdashboard/GtwDashboardApplication.java
+++ src/main/java/com/gtwdashboard/GtwDashboardApplication.java
@@ -2,8 +2,11 @@ package com.gtwdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableHystrixDashboard
 public class GtwDashboardApplication {
 
 	public static void main(String[] args) {
