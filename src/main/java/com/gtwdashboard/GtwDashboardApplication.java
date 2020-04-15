d3aa339458333fe2de1c428117378adfe1aafd26
batro83 <batro_ska@yahoo.es>
2020-04-15 19:02:15 +0200

    Initial commit

---
+++ src/main/java/com/gtwdashboard/GtwDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.gtwdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtwDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtwDashboardApplication.class, args);
+	}
+
+}
